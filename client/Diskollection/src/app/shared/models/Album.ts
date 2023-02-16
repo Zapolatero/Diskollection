f25c0654e81b4f4964018a43c6051e66460ef780
Louis PASCUZZI <pascuzzi.l@sfeir.com>
2023-02-16 13:17:52 +0100

    creation of AlbumDTO model

--- client/Diskollection/src/app/shared/models/Album.ts
+++ client/Diskollection/src/app/shared/models/Album.ts
@@ -9,3 +9,13 @@ export interface Album{
     description?: string;
     likes: 0;
 }
+
+export interface AlbumDTO{
+    id?: string;
+    title: string;
+    artistId: string;
+    releaseDate: string;
+    genre: string;
+    imageURL?: string;
+    description?: string;
+}
